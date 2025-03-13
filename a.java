0548ef4b13e355166bfe6fcddad8b1bfa5a51284
rohan <rohanmr551@gmail.com>
2025-03-14 00:21:41 +0530

    third commit

--- a.java
+++ a.java
@@ -1,8 +1,11 @@
 class a{
 	public static void main(String rgs[]){
-		
-			int a=10;
-			int b=20;
-			System.out.println(a+b+"hello");
+		add();
+	}
+
+	static void add(){
+		int a=10;
+		int b=20;
+		System.out.println(a+b+"hello");
 	}
 }
